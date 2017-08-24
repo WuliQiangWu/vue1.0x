d8ffc71204436afac6438da0c451b3722c649489
AJ <aj@AJdeMacBook-Pro.local>
2017-08-24 17:57:39 +0800

    first push

---
+++ src/components/seller/seller.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    this is a vue seller
+  </div>
+</template>
+<script>
+  export default {
+    name: '',
+    data () {
+      return {
+        msg: ''
+      };
+    },
+    components: {}
+  };
+</script>
